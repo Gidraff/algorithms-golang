c1fb5a38378381bf703132895b53dbf7cb3b5c44
gidraff <kamandegid@gmail.com>
2020-02-25 12:01:43 +0300

    feat(print): print fizz or buzz - print fizz or buzz if a number is divisible by 3 or 5 # FizzBuzz

--- main.go
+++ main.go
@@ -9,4 +9,11 @@ import (
 func main() {
 	numList := []int{1, 2, 3, 4, 5, 5}
 	fmt.Println(numeric.NumInList(numList, 5))
+
+	fmt.Print("Fizz Buzz Algo")
+	fmt.Println()
+	numeric.FizzBuzz(15)
+
+	fmt.Println("DecToBase==>")
+	fmt.Print(numeric.DecToBase(10, 2))
 }
--- numeric/fizz_buzz.go
+++ numeric/fizz_buzz.go
@@ -1,6 +1,23 @@
 package numeric
 
+import "fmt"
+
 // FizzBuzz prints FizzBuzz
 func FizzBuzz(n int) {
-	return
+	for i := 1; i <= n; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Print("Fizz Buzz")
+		case i%3 == 0:
+			fmt.Print("Fizz")
+		case i%5 == 0:
+			fmt.Print("Buzz")
+		default:
+			fmt.Print(n)
+		}
+		if i != n {
+			fmt.Print(". ")
+		}
+	}
+	fmt.Println()
 }
