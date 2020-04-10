944530eba4db35d3ded181c5a58ceef66d385a18
Gidraff <kamandegid@gmail.com>
2020-04-10 14:31:29 +0300

    chore(docs): document bubblesort algo

--- sorting/bubble_sort.go
+++ sorting/bubble_sort.go
@@ -2,6 +2,11 @@ package sorting
 
 import "fmt"
 
+// Iterates through a list, comparing the first element and the second element.
+// if the first element is larger than the second element, it swaps them.
+// It performs this action for the entire list. BubbleSort TC is O(N^2)
+
+// BubbleSortInt sorts a list of integers
 func BubbleSortInt(list []int) {
 	for sweepNum := 0; sweepNum < len(list); sweepNum++ {
 		swapped := false
